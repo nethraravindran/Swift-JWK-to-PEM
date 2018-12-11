d4d827fe05af3f88d812bdb64a0fc8cadc5f0051
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-12-11 19:54:38 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SwiftJWKtoPEM"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nethraravindran/OpenSSL.git", .branch("openssl-11")),
+        .package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SwiftJWKtoPEM"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nethraravindran/OpenSSL.git", .branch("toolsVersion40")),
+        .package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
